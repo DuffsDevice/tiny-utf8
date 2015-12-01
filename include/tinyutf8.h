852ab344db26ca61b0d4fcbb41e5faa51a26e524
ijake1111 <ijake1111@a14fc95e-7a6c-4dd7-bef0-c5c03e6aa7be>
2015-12-01 13:08:56 +0000

    Adjusted the Version number inside tinyutf8.h and Added release zip file

--- include/tinyutf8.h
+++ include/tinyutf8.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2015 Jakob Riedle (DuffsDevice)
 // 
-// Version 1.0
+// Version 1.1
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
