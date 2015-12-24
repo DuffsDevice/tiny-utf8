009ab4e9da23e58ecb7691adabaeaf4a41289b84
ijake1111 <ijake1111@a14fc95e-7a6c-4dd7-bef0-c5c03e6aa7be>
2015-12-24 13:39:22 +0000

    Fixed version string in tinyutf8.h

--- include/tinyutf8.h
+++ include/tinyutf8.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2015 Jakob Riedle (DuffsDevice)
 // 
-// Version 1.1
+// Version 1.2a
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
