50ebfd04f73be1bc80f152a04c7f71321f7fef98
ijake1111 <13666193+DuffsDevice@users.noreply.github.com>
2016-05-21 13:46:03 +0000

    ~ Fixed a class interface error, where iterators could be implicitly created using an integer that would then be interpreted as a byte index rather that a codepoint position ~ Speeded up get_num_bytes_of_utf8_char and get_num_bytes_of_utf8_char_before ~ Fixed major bugs, when comparing char's in a signed way (all over the place) ~ Made ANSI work correctly now, in that way, that every byte within an incomplete utf8 multibyte sequence would be interpreted as a single character + Added some unit tests within test.cpp

--- source/test.cpp
+++ source/test.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main(int argc, char ** argv)
 {
-	std::ofstream fout = ofstream( "test.txt" , std::ios::binary );
+	ofstream fout = ofstream( "test.txt" , std::ios::binary );
 	
 	// Prepend the UTF-8 Byte Order Mark
 	fout << utf8_string().cpp_str(true);
@@ -71,7 +71,88 @@ int main(int argc, char ** argv)
 					fout << "U\\" << int(cp);
 			}
 		);
+		
+		fout << endl << endl;
 	}
+	
+	// Test 4
+	{
+		fout << "Test 4: " << endl << "-------" << endl;
+		
+		utf8_string str = utf8_string( U"Hello ツ World ♫" );
+		
+		fout << "String: " << str << endl;
+		
+		str[6] = '~';
+		
+		fout << "Replaced codepoint 6 with ~: " << str << endl;
+		
+		str.replace( 5 , 3 , " " );
+		
+		fout << "Replaced codepoints 5-7 with ' ': " << str << endl;
+		
+		str.insert( begin(str) , utf8_string(U"ツ ") );
+		
+		fout << "Inserted at the start 'ツ ': " << str << endl << endl;
+	}
+	
+	// Test 4
+	{
+		fout << "Test 4: " << endl << "-------" << endl;
+		
+		utf8_string str = utf8_string( U"Hello ツ World ♫" );
+		
+		fout << "String: " << str << endl;
+		
+		str.erase( 14 );
+		
+		fout << "Erased codepoint 14: " << str << endl;
+		
+		str.erase( str.begin() , str.begin() + 6 );
+		
+		fout << "Erased codepoints 0 to 6: " << str << endl << endl;
+	}
+	
+	// Test 5
+	{
+		fout << "Test 5: " << endl << "-------" << endl;
+		
+		std::ifstream in = ifstream( "input.txt" , ios::binary );
+		
+		utf8_string str;
+		
+		in >> str;
+		
+		fout << "String (ANSI): " << str << endl;
+		fout << "MisFormated: " << str.is_misformated() << endl;
+		
+		fout << "Iterative Output: ";
+		
+		std::for_each( begin(str) , end(str)
+			, [&fout]( char32_t cp ){
+				if( cp <= 127 )
+					fout << char(cp);
+				else
+					fout << "U\\" << int(cp);
+			}
+		);
+		
+		fout << endl;
+		
+		fout << "Reverse Iterative Output: ";
+		
+		std::for_each( rbegin(str) , rend(str)
+			, [&fout]( char32_t cp ){
+				if( cp <= 127 )
+					fout << char(cp);
+				else
+					fout << "U\\" << int(cp);
+			}
+		);
+		
+		fout << endl;
+	}
+	
 	
 	return 0;
 }
