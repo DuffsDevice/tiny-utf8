c01f96112e0376a534beebbde9c2867735e7785d
Jakob Riedle <jakob.riedle@gmx.de>
2020-05-31 23:30:54 +0200

    Fixed use of [[fallthrough]] if lower than C++17

--- tinyutf8.h
+++ tinyutf8.h
@@ -52,6 +52,13 @@
 	#endif
 #endif
 
+//! Determine the way to inform about fallthrough behavior
+#if __cplusplus >= 201700L
+	#define FALLTHROUGH_INTENTIONAL [[fallthrough]];
+#else
+	#define FALLTHROUGH_INTENTIONAL
+#endif
+
 //! Remove Warning "-Wmaybe-uninitialized" (GCC/Clang) resp. "C4703" (MSVC), since it is wrong for all cases in this file
 #if defined (__GNUC__)
 #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
@@ -666,11 +673,11 @@ private: //! Static helper methods
 	 */
 	inline static void					encode_utf8( value_type cp , char* dest , width_type cp_bytes ){
 		switch( cp_bytes ){
-			case 7: dest[cp_bytes-6] = 0x80 | ((cp >> 30) & 0x3F); [[fallthrough]];
-			case 6: dest[cp_bytes-5] = 0x80 | ((cp >> 24) & 0x3F); [[fallthrough]];
-			case 5: dest[cp_bytes-4] = 0x80 | ((cp >> 18) & 0x3F); [[fallthrough]];
-			case 4: dest[cp_bytes-3] = 0x80 | ((cp >> 12) & 0x3F); [[fallthrough]];
-			case 3: dest[cp_bytes-2] = 0x80 | ((cp >>  6) & 0x3F); [[fallthrough]];
+			case 7: dest[cp_bytes-6] = 0x80 | ((cp >> 30) & 0x3F); FALLTHROUGH_INTENTIONAL
+			case 6: dest[cp_bytes-5] = 0x80 | ((cp >> 24) & 0x3F); FALLTHROUGH_INTENTIONAL
+			case 5: dest[cp_bytes-4] = 0x80 | ((cp >> 18) & 0x3F); FALLTHROUGH_INTENTIONAL
+			case 4: dest[cp_bytes-3] = 0x80 | ((cp >> 12) & 0x3F); FALLTHROUGH_INTENTIONAL
+			case 3: dest[cp_bytes-2] = 0x80 | ((cp >>  6) & 0x3F); FALLTHROUGH_INTENTIONAL
 			case 2: dest[cp_bytes-1] = 0x80 | ((cp >>  0) & 0x3F);
 				dest[0] = (unsigned char)( ( std::uint_least16_t(0xFF00uL) >> cp_bytes ) | ( cp >> ( 6 * cp_bytes - 6 ) ) );
 				break;
@@ -2243,15 +2250,15 @@ utf8_string::utf8_string( const char* str , size_type len , tiny_utf8_detail::re
 				width_type bytes = get_codepoint_bytes( *str_iter , str_end - str_iter );
 				switch( bytes )
 				{
-					case 7:	buffer_iter[6] = str_iter[6]; [[fallthrough]]; // Copy data byte
-					case 6:	buffer_iter[5] = str_iter[5]; [[fallthrough]]; // Copy data byte
-					case 5:	buffer_iter[4] = str_iter[4]; [[fallthrough]]; // Copy data byte
-					case 4:	buffer_iter[3] = str_iter[3]; [[fallthrough]]; // Copy data byte
-					case 3:	buffer_iter[2] = str_iter[2]; [[fallthrough]]; // Copy data byte
+					case 7:	buffer_iter[6] = str_iter[6]; FALLTHROUGH_INTENTIONAL // Copy data byte
+					case 6:	buffer_iter[5] = str_iter[5]; FALLTHROUGH_INTENTIONAL // Copy data byte
+					case 5:	buffer_iter[4] = str_iter[4]; FALLTHROUGH_INTENTIONAL // Copy data byte
+					case 4:	buffer_iter[3] = str_iter[3]; FALLTHROUGH_INTENTIONAL // Copy data byte
+					case 3:	buffer_iter[2] = str_iter[2]; FALLTHROUGH_INTENTIONAL // Copy data byte
 					case 2:	buffer_iter[1] = str_iter[1]; // Copy data byte
 						// Set next entry in the LUT!
 						utf8_string::set_lut( lut_iter -= lut_width , lut_width , str_iter - str );
-						[[fallthrough]];
+						FALLTHROUGH_INTENTIONAL
 					case 1: buffer_iter[0] = str_iter[0]; break; // Copy data byte
 				}
 				buffer_iter	+= bytes;
@@ -2339,15 +2346,15 @@ utf8_string::utf8_string( const char* str , size_type data_len , tiny_utf8_detai
 				width_type bytes = get_codepoint_bytes( *str_iter , str_end - str_iter );
 				switch( bytes )
 				{
-					case 7:	buffer_iter[6] = str_iter[6]; [[fallthrough]]; // Copy data byte
-					case 6:	buffer_iter[5] = str_iter[5]; [[fallthrough]]; // Copy data byte
-					case 5:	buffer_iter[4] = str_iter[4]; [[fallthrough]]; // Copy data byte
-					case 4:	buffer_iter[3] = str_iter[3]; [[fallthrough]]; // Copy data byte
-					case 3:	buffer_iter[2] = str_iter[2]; [[fallthrough]]; // Copy data byte
+					case 7:	buffer_iter[6] = str_iter[6]; FALLTHROUGH_INTENTIONAL // Copy data byte
+					case 6:	buffer_iter[5] = str_iter[5]; FALLTHROUGH_INTENTIONAL // Copy data byte
+					case 5:	buffer_iter[4] = str_iter[4]; FALLTHROUGH_INTENTIONAL // Copy data byte
+					case 4:	buffer_iter[3] = str_iter[3]; FALLTHROUGH_INTENTIONAL // Copy data byte
+					case 3:	buffer_iter[2] = str_iter[2]; FALLTHROUGH_INTENTIONAL // Copy data byte
 					case 2:	buffer_iter[1] = str_iter[1]; // Copy data byte
 						// Set next entry in the LUT!
 						utf8_string::set_lut( lut_iter -= lut_width , lut_width , str_iter - str );
-						[[fallthrough]];
+						FALLTHROUGH_INTENTIONAL
 					case 1: buffer_iter[0] = str_iter[0]; break; // Copy data byte
 				}
 				buffer_iter	+= bytes;
@@ -2492,27 +2499,27 @@ utf8_string::width_type utf8_string::get_num_bytes_of_utf8_char_before( const ch
 		default:
 			if( ((unsigned char)data_start[-7] & 0xFE ) == 0xFC )	// 11111110 seven bytes
 				return 7;
-			[[fallthrough]];
+			FALLTHROUGH_INTENTIONAL
 		case 6:
 			if( ((unsigned char)data_start[-6] & 0xFE ) == 0xFC )	// 1111110X six bytes
 				return 6;
-			[[fallthrough]];
+			FALLTHROUGH_INTENTIONAL
 		case 5:
 			if( ((unsigned char)data_start[-5] & 0xFC ) == 0xF8 )	// 111110XX five bytes
 				return 5;
-			[[fallthrough]];
+			FALLTHROUGH_INTENTIONAL
 		case 4:
 			if( ((unsigned char)data_start[-4] & 0xF8 ) == 0xF0 )	// 11110XXX four bytes
 				return 4;
-			[[fallthrough]];
+			FALLTHROUGH_INTENTIONAL
 		case 3:
 			if( ((unsigned char)data_start[-3] & 0xF0 ) == 0xE0 )	// 1110XXXX three bytes
 				return 3;
-			[[fallthrough]];
+			FALLTHROUGH_INTENTIONAL
 		case 2:
 			if( ((unsigned char)data_start[-2] & 0xE0 ) == 0xC0 )	// 110XXXXX two bytes
 				return 2;
-			[[fallthrough]];
+			FALLTHROUGH_INTENTIONAL
 		case 1:
 		case 0:
 			return 1;
@@ -2612,7 +2619,7 @@ utf8_string& utf8_string::operator=( const utf8_string& str )
 		lbl_replicate_whole_buffer: // Replicate the whole buffer
 			delete[] t_non_sso.data;
 		}
-			[[fallthrough]];
+			FALLTHROUGH_INTENTIONAL
 		case 2: // [sso-active] = [sso-inactive]
 			t_non_sso.data = new char[ utf8_string::determine_total_buffer_size( str.t_non_sso.buffer_size ) ];
 			std::memcpy( t_non_sso.data , str.t_non_sso.data , str.t_non_sso.buffer_size + sizeof(indicator_type) ); // Copy data
@@ -2622,7 +2629,7 @@ utf8_string& utf8_string::operator=( const utf8_string& str )
 			return *this;
 		case 1: // [sso-inactive] = [sso-active]
 			delete[] t_non_sso.data;
-			[[fallthrough]];
+			FALLTHROUGH_INTENTIONAL
 		case 0: // [sso-active] = [sso-active]
 			if( &str != this )
 				std::memcpy( (void*)this , &str , sizeof(utf8_string) ); // Copy data
