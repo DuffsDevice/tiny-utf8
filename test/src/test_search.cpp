9274ad83c5c11dd7dc8a6683892cb2e13f30c435
Jakob Riedle <jakob.riedle@gmx.de>
2020-10-26 17:13:29 +0100

    Added basic_utf8_string::starts_with/ends_with (#54)
    * Added basic_utf8_string::starts_with/ends_with
    
    * Added test cases for starts_with and ends_with
    
    Co-authored-by: Jakob Riedle <riedle@validas.de>

--- test/src/test_search.cpp
+++ test/src/test_search.cpp
@@ -16,3 +16,22 @@ TEST(TinyUTF8, FindSubstr)
 	EXPECT_EQ(str.find(find), 2);
 	EXPECT_EQ(str.rfind(rfind), 9);
 }
+
+TEST(TinyUTF8, StartsEndsWith)
+{
+	tiny_utf8::utf8_string str = U"Hello World ツ♫";
+
+	const char32_t* ends_with_positive = U"ツ♫";
+	const char32_t* ends_with_negative = U"e";
+	const char32_t* starts_with_positive = U"Hello ";
+	const char32_t* starts_with_negative = U"Hell ";
+
+	EXPECT_EQ(str.ends_with(ends_with_positive), true);
+	EXPECT_EQ(str.ends_with(ends_with_negative), false);
+	EXPECT_EQ(str.ends_with(tiny_utf8::utf8_string(ends_with_positive)), true);
+	EXPECT_EQ(str.ends_with(tiny_utf8::utf8_string(ends_with_negative)), false);
+	EXPECT_EQ(str.starts_with(starts_with_positive), true);
+	EXPECT_EQ(str.starts_with(starts_with_negative), false);
+	EXPECT_EQ(str.starts_with(tiny_utf8::utf8_string(starts_with_positive)), true);
+	EXPECT_EQ(str.starts_with(tiny_utf8::utf8_string(starts_with_negative)), false);
+}
